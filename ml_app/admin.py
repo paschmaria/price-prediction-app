219363f27b95ec921be470adc83f522ec3b680e2
Paschal Onuorah <onuorahpaschal@gmail.com>
2020-05-07 23:07:08 +0100

    Created model for Car Instance

--- ml_app/admin.py
+++ ml_app/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin
+import csv
+import datetime
 
-# Register your models here.
+from django.contrib import admin
+from django.http import HttpResponse
+
+from .models import Car
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name_plural.capitalize()}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not \
+                field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+
+        for field in fields:
+            value = getattr(obj, field.name)
+
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+export_to_csv.short_description = 'Export to CSV'
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'model', 'fuel_type', 
+                    'transmission', 'power', 'seats',
+                    'price']
+    list_filter = ['brand', 'model']
+    actions = [export_to_csv]
--- ml_app/models.py
+++ ml_app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Car(models.Model):
+    """
+    Instance of used car
+    """
+
+    slug = models.SlugField(max_length=200, unique=True)
+    brand = models.CharField(_("car brand"), max_length=50)
+    model = models.CharField(_("car model"), max_length=50)
+    location = models.CharField(_("car location"), max_length=50)
+    year = models.PositiveSmallIntegerField(_("model year"))
+    km_driven = models.PositiveIntegerField(_("kilometer driven"))
+    fuel_type = models.CharField(_("fuel type"), max_length=50)
+    transmission = models.CharField(_("transmission"), max_length=50)
+    owner_type = models.CharField(_("owner type"), max_length=50)
+    mileage = models.DecimalField(_("car mileage"), max_digits=10, decimal_places=2)
+    engine = models.DecimalField(_("engine size"), max_digits=10, decimal_places=2)
+    power = models.DecimalField(_("engine power"), max_digits=10, decimal_places=2)
+    seats = models.FloatField(_("seating capacity"))
+    price = models.DecimalField(_("car price (N)"), max_digits=12, decimal_places=2)
+
+    class Meta:
+        ordering = ('brand', 'model')
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} {self.year}"
+
+    def get_slug(self):
+        slug = slugify(f"{self.brand} {self.model} {self.year}")
+        return slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.get_slug()
+        return super().save(*args, **kwargs)
