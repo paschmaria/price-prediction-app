b740694869046eb703d75355045d0d97c37ff3e6
Paschal Onuorah <onuorahpaschal@gmail.com>
2020-05-14 10:56:24 +0100

    Updated API to render only as JSON

--- core/settings/base.py
+++ core/settings/base.py
@@ -143,6 +143,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 
